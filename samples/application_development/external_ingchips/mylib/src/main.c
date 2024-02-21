3b61d7559930d9434164cdacf6fc3ee65c29b10d
zyx <yuxizhangyuxi@gmail.com>
2024-02-21 19:51:10 +0800

    [feature]:add main profile

--- samples/application_development/external_ingchips/mylib/src/main.c
+++ samples/application_development/external_ingchips/mylib/src/main.c
@@ -1,18 +1,214 @@
 #include <stdio.h>
 #include <string.h>
+#include "profile.h"
 #include "ingsoc.h"
 #include "platform_api.h"
-#include "profile.h"
-#include "main_shared.c"
+#include "port_gen_os_driver.h"
+#include "trace.h"
+#include "../data/setup_soc.cgen"
 #include <zephyr/kernel.h>
-// #include <zephyr/types.h>
-// #include <stddef.h>
-// // #include <zephyr/sys/printk.h>
-// // #include <zephyr/sys/util.h>
+static uint32_t cb_hard_fault(hard_fault_info_t *info, void *_)
+{
+    platform_printf("HARDFAULT:\nPC : 0x%08X\nLR : 0x%08X\nPSR: 0x%08X\n"
+                    "R0 : 0x%08X\nR1 : 0x%08X\nR2 : 0x%08X\nP3 : 0x%08X\n"
+                    "R12: 0x%08X\n",
+                    info->pc, info->lr, info->psr,
+                    info->r0, info->r1, info->r2, info->r3, info->r12);
+    for (;;);
+}
 
-// // #include <zephyr/bluetooth/bluetooth.h>
-// // #include <zephyr/bluetooth/hci.h>
-// // #include "../../peripheral_console/src/key_detector.h"
+static uint32_t cb_assertion(assertion_info_t *info, void *_)
+{
+    platform_printf("[ASSERTION] @ %s:%d\n",
+                    info->file_name,
+                    info->line_no);
+    for (;;);
+}
+
+static uint32_t cb_heap_out_of_mem(uint32_t tag, void *_)
+{
+    platform_printf("[OOM] @ %d\n", tag);
+    for (;;);
+}
+
+#define PRINT_PORT    APB_UART0
+
+uint32_t cb_putc(char *c, void *dummy)
+{
+    while (apUART_Check_TXFIFO_FULL(PRINT_PORT) == 1);
+    UART_SendData(PRINT_PORT, (uint8_t)*c);
+    while (apUART_Check_TXFIFO_FULL(APB_UART1) == 1);
+    UART_SendData(APB_UART1, (uint8_t)*c);
+    return 0;
+}
+
+int fputc(int ch, FILE *f)
+{
+    cb_putc((char *)&ch, NULL);
+    return ch;
+}
+static uint32_t uart_isr0(void *user_data)
+{
+    // __disable_irq();
+    // while(1);
+    uint32_t status;
+
+    while(1)
+    {
+        status = apUART_Get_all_raw_int_stat(APB_UART0);
+        if (status == 0)
+            break;
+
+        APB_UART0->IntClear = status;
+
+        while (apUART_Check_RXFIFO_EMPTY(APB_UART0) != 1)
+        {
+            char c = APB_UART0->DataRead;
+            platform_printf("%c", c);
+            // console_rx_data(&c, 1);
+        }
+    }
+    return 0;
+}
+
+static uint32_t uart_isr1(void *user_data)
+{
+    // __disable_irq();
+    // while(1);
+    uint32_t status;
+
+    while(1)
+    {
+        status = apUART_Get_all_raw_int_stat(APB_UART1);
+        if (status == 0)
+            break;
+
+        APB_UART1->IntClear = status;
+
+        while (apUART_Check_RXFIFO_EMPTY(APB_UART1) != 1)
+        {
+            char c = APB_UART1->DataRead;
+            platform_printf("%c", c);
+            // console_rx_data(&c, 1);
+        }
+    }
+    return 0;
+}
+void config_user_uart(uint32_t freq, uint32_t baud)
+{
+    UART_sStateStruct config;
+
+    config.word_length       = UART_WLEN_8_BITS;
+    config.parity            = UART_PARITY_NOT_CHECK;
+    config.fifo_enable       = 1;
+    config.two_stop_bits     = 0;
+    config.receive_en        = 1;
+    config.transmit_en       = 1;
+    config.UART_en           = 1;
+    config.cts_en            = 0;
+    config.rts_en            = 0;
+    config.rxfifo_waterlevel = 4;
+    config.txfifo_waterlevel = 1;
+    config.ClockFrequency    = freq;
+    config.BaudRate          = baud;
+
+    apUART_Initialize(APB_UART0, &config, (1 << bsUART_RECEIVE_INTENAB) | (1 << bsUART_TIMEOUT_INTENAB));
+    apUART_Initialize(APB_UART1, &config, (1 << bsUART_RECEIVE_INTENAB) | (1 << bsUART_TIMEOUT_INTENAB));
+
+#ifdef TRACE_TO_UART
+    //config.BaudRate          = 921600;
+    apUART_Initialize(TRACE_PORT, &config, 1 << bsUART_TRANSMIT_INTENAB);
+#endif
+}
+void setup_peripherals(void)
+{
+     cube_setup_peripherals();//
+     config_user_uart(OSC_CLK_FREQ, 115200);
+    platform_set_irq_callback(PLATFORM_CB_IRQ_UART0, uart_isr0, NULL);
+    platform_set_irq_callback(PLATFORM_CB_IRQ_UART1, uart_isr1, NULL);
+//platform_set_irq_callback(PLATFORM_CB_IRQ_GPIO, (f_platform_irq_cb)&gpio_isr, NULL);
+}
+
+uint32_t on_lle_init(void *dummy, void *user_data)
+{
+    (void)(dummy);
+    (void)(user_data);
+    cube_on_lle_init();
+    return 0;
+}
+
+uint32_t on_deep_sleep_wakeup(void *dummy, void *user_data)
+{
+    (void)(dummy);
+    (void)(user_data);
+    setup_peripherals();
+    return 0;
+}
+
+uint32_t query_deep_sleep_allowed(void *dummy, void *user_data)
+{
+    (void)(dummy);
+    (void)(user_data);
+    // TODO: return 0 if deep sleep is not allowed now; else deep sleep is allowed
+    return 0;
+}
+
+trace_rtt_t trace_ctx = {0};
+
+
+static const platform_evt_cb_table_t evt_cb_table =
+{
+    .callbacks = {
+        [PLATFORM_CB_EVT_HARD_FAULT] = {
+            .f = (f_platform_evt_cb)cb_hard_fault,
+        },
+        [PLATFORM_CB_EVT_ASSERTION] = {
+            .f = (f_platform_evt_cb)cb_assertion,
+        },
+        [PLATFORM_CB_EVT_HEAP_OOM] = {
+            .f = (f_platform_evt_cb)cb_heap_out_of_mem,
+        },
+        [PLATFORM_CB_EVT_PROFILE_INIT] = {
+            .f = setup_profile,
+        },
+        [PLATFORM_CB_EVT_LLE_INIT] = {
+            .f = on_lle_init,
+        },
+        [PLATFORM_CB_EVT_ON_DEEP_SLEEP_WAKEUP] = {
+            .f = (f_platform_evt_cb)on_deep_sleep_wakeup,
+        },
+        [PLATFORM_CB_EVT_QUERY_DEEP_SLEEP_ALLOWED] = {
+            .f = query_deep_sleep_allowed,
+        },
+        [PLATFORM_CB_EVT_PUTC] = {
+            .f = (f_platform_evt_cb)cb_putc,
+        },
+        [PLATFORM_CB_EVT_TRACE] = {
+            .f = (f_platform_evt_cb)cb_trace_rtt,
+            .user_data = &trace_ctx,
+        },
+    }
+};
+
+// TODO: add RTOS source code to the project.
+uintptr_t app_main()
+{
+    cube_soc_init();
+    
+    // setup event handlers
+    platform_set_evt_callback_table(&evt_cb_table);
+
+    setup_peripherals();
+    SYSCTRL_Init();
+
+    trace_rtt_init(&trace_ctx);
+    // TODO: config trace mask
+    platform_config(PLATFORM_CFG_TRACE_MASK, 0);
+    return (uintptr_t)os_impl_get_driver();
+}
+
+
+
 struct k_thread test_thread;
 #define TEST_STACK_SIZE 1024
 K_KERNEL_STACK_DEFINE(test_thread_stack, TEST_STACK_SIZE);
@@ -29,20 +225,6 @@ void my_thread_func(void *p1, void *p2, void *p3) {
         printk("printk print out ok\r\n");
     }
 }
-// extern const void *os_impl_get_driver(void);
-
-
-int app_main()
-{
-    _app_main();
-    platform_printf("I am in app_main\r\n");
-
-#if (INGCHIPS_FAMILY == INGCHIPS_FAMILY_918)
-#elif (INGCHIPS_FAMILY == INGCHIPS_FAMILY_916)
-    platform_config(PLATFORM_CFG_DEEP_SLEEP_TIME_REDUCTION, 4500);
-#endif
-    return (uintptr_t)os_impl_get_driver();
-}
 void main() {
     k_tid_t tid = k_thread_create(&test_thread,          // 线程对象
                                  test_thread_stack,
@@ -57,13 +239,14 @@ void main() {
 
     if (tid == 0) {
         printk("无法创建线程\n");
-    } else {
+    } else { 
         printk("成功创建线程\n");
     }
     // port_task_create( "test",create_task_test, NULL,1024, 5);
     // k_timer_init(&test_timer, test_time_cb, NULL);
     // k_timer_start(&test_timer, K_MSEC(1000), K_MSEC(1000));
     os_impl_task_create_real();
+    
     while(1) {
         static uint8_t i = 8;
         const char* senddata="send hello\r\n";
--- samples/application_development/external_ingchips/mylib/src/profile.c
+++ samples/application_development/external_ingchips/mylib/src/profile.c
@@ -1,13 +1,9 @@
-#include <stddef.h>
 #include <stdio.h>
-#include <string.h>
 #include "platform_api.h"
 #include "att_db.h"
 #include "gap.h"
 #include "btstack_event.h"
-
-#include "service_console.h"
-#include "key_detector.h"
+#include "btstack_defines.h"
 
 // GATT characteristic handles
 #include "../data/gatt.const"
@@ -16,18 +12,17 @@ const static uint8_t adv_data[] = {
     #include "../data/advertising.adv"
 };
 
+#include "../data/advertising.const"
+
 const static uint8_t scan_data[] = {
     #include "../data/scan_response.adv"
 };
 
+#include "../data/scan_response.const"
+
 const static uint8_t profile_data[] = {
     #include "../data/gatt.profile"
 };
-
-hci_con_handle_t handle_send = 0;
-static uint8_t notify_enable = 0;
-
-extern const char welcome_msg[];
 
 static uint16_t att_read_callback(hci_con_handle_t connection_handle, uint16_t att_handle, uint16_t offset,
                                   uint8_t * buffer, uint16_t buffer_size)
@@ -40,114 +35,42 @@ static uint16_t att_read_callback(hci_con_handle_t connection_handle, uint16_t a
     }
 }
 
-static btstack_packet_callback_registration_t hci_event_callback_registration;
-
 static int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_handle, uint16_t transaction_mode,
                               uint16_t offset, const uint8_t *buffer, uint16_t buffer_size)
 {
     switch (att_handle)
     {
-    case HANDLE_GENERIC_INPUT:
-        console_rx_data((const char *)buffer, buffer_size);
-        return 0;
-    case HANDLE_GENERIC_OUTPUT + 1:
-        handle_send = connection_handle;
-        if(*(uint16_t *)buffer == GATT_CLIENT_CHARACTERISTICS_CONFIGURATION_NOTIFICATION)
-        {
-            notify_enable = 1;
-            att_server_notify(handle_send, HANDLE_GENERIC_OUTPUT, (uint8_t *)welcome_msg, strlen(welcome_msg) + 1);
-        }
-        else
-            notify_enable = 0;
-        return 0;
+
     default:
         return 0;
     }
 }
-
-#define USER_MSG_ID_REQUEST_SEND            1
-#define USER_MSG_ID_KEY_EVENT               2
-#define USER_MSG_ID_FIRST_WAKE_UP           3
-
-void stack_notify_tx_data(void)
-{
-    if (notify_enable)
-        btstack_push_user_msg(USER_MSG_ID_REQUEST_SEND, NULL, 0);
-}
-
-void stack_on_first_wake_up(void)
-{
-    btstack_push_user_msg(USER_MSG_ID_FIRST_WAKE_UP, NULL, 0);
-}
-
-void on_key_event(key_press_event_t evt)
-{
-    btstack_push_user_msg(USER_MSG_ID_KEY_EVENT, NULL, evt);
-}
-
-void stack_set_latency(int latency)
-{
-    ll_set_conn_latency(handle_send, latency);
-}
-
-const static ext_adv_set_en_t adv_sets_en[1] = {{.handle = 0, .duration = 0, .max_events = 0}};
 
 static void user_msg_handler(uint32_t msg_id, void *data, uint16_t size)
 {
     switch (msg_id)
     {
-    case USER_MSG_ID_REQUEST_SEND:
-        if (notify_enable)
-        {
-            uint16_t len;
-            uint8_t *data = console_get_clear_tx_data(&len);
-            uint16_t mtu = att_server_get_mtu(handle_send) - 3;
-            while (len > 0)
-            {
-                uint16_t block = len > mtu ? mtu : len;
-                att_server_notify(handle_send, HANDLE_GENERIC_OUTPUT, data, block);
-                data += block;
-                len -= block;
-            }
-        }
-        break;
-    case USER_MSG_ID_KEY_EVENT:
-        {
-            static char str[50];
-            key_press_event_t evt = (key_press_event_t)size;
-            if (evt == KEY_LONG_PRESSED)
-                strcpy(str, "key long pressed\n");
-            else
-                sprintf(str, "key pressed %d times\n", evt);
-
-            if (notify_enable)
-                att_server_notify(handle_send, HANDLE_GENERIC_OUTPUT, (uint8_t *)str, strlen(str) + 1);
-        }
-        break;
-    case USER_MSG_ID_FIRST_WAKE_UP:
-        platform_32k_rc_auto_tune();
-        gap_set_ext_adv_enable(1, sizeof(adv_sets_en) / sizeof(adv_sets_en[0]), adv_sets_en);
-        break;
+        // add your code
+    //case MY_MESSAGE_ID:
+    //    break;
     default:
         ;
     }
 }
 
-extern int adv_tx_power;
+const static ext_adv_set_en_t adv_sets_en[] = { {.handle = 0, .duration = 0, .max_events = 0} };
 
-static void setup_adv()
+static void setup_adv(void)
 {
-    const static bd_addr_t rand_addr = {0xFB, 0x89, 0x67, 0x45, 0x23, 0x01};
-    gap_set_adv_set_random_addr(0, rand_addr);
     gap_set_ext_adv_para(0,
                             CONNECTABLE_ADV_BIT | SCANNABLE_ADV_BIT | LEGACY_PDU_BIT,
-                            800, 800,                  // Primary_Advertising_Interval_Min, Primary_Advertising_Interval_Max
+                            0x00a1, 0x00a1,            // Primary_Advertising_Interval_Min, Primary_Advertising_Interval_Max
                             PRIMARY_ADV_ALL_CHANNELS,  // Primary_Advertising_Channel_Map
                             BD_ADDR_TYPE_LE_RANDOM,    // Own_Address_Type
                             BD_ADDR_TYPE_LE_PUBLIC,    // Peer_Address_Type (ignore)
                             NULL,                      // Peer_Address      (ignore)
                             ADV_FILTER_ALLOW_ALL,      // Advertising_Filter_Policy
-                            adv_tx_power,              // Advertising_Tx_Power
+                            0x00,                      // Advertising_Tx_Power
                             PHY_1M,                    // Primary_Advertising_PHY
                             0,                         // Secondary_Advertising_Max_Skip
                             PHY_1M,                    // Secondary_Advertising_PHY
@@ -155,10 +78,12 @@ static void setup_adv()
                             0x00);                     // Scan_Request_Notification_Enable
     gap_set_ext_adv_data(0, sizeof(adv_data), (uint8_t*)adv_data);
     gap_set_ext_scan_response_data(0, sizeof(scan_data), (uint8_t*)scan_data);
+    gap_set_ext_adv_enable(1, sizeof(adv_sets_en) / sizeof(adv_sets_en[0]), adv_sets_en);
 }
 
 static void user_packet_handler(uint8_t packet_type, uint16_t channel, const uint8_t *packet, uint16_t size)
 {
+    static const bd_addr_t rand_addr = { 0xFA, 0xD3, 0xE7, 0xD8, 0x81, 0x17 };
     uint8_t event = hci_event_packet_get_type(packet);
     const btstack_user_msg_t *p_user_msg;
     if (packet_type != HCI_EVENT_PACKET) return;
@@ -168,9 +93,19 @@ static void user_packet_handler(uint8_t packet_type, uint16_t channel, const uin
     case BTSTACK_EVENT_STATE:
         if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING)
             break;
-        platform_config(PLATFORM_CFG_LL_LEGACY_ADV_INTERVAL, (1250 << 16) | 750);
+        gap_set_adv_set_random_addr(0, rand_addr);
         setup_adv();
-        platform_set_timer(stack_on_first_wake_up, 200);
+        break;
+
+    case HCI_EVENT_COMMAND_COMPLETE:
+        switch (hci_event_command_complete_get_command_opcode(packet))
+        {
+        // add your code to check command complete response
+        // case :
+        //     break;
+        default:
+            break;
+        }
         break;
 
     case HCI_EVENT_LE_META:
@@ -179,16 +114,6 @@ static void user_packet_handler(uint8_t packet_type, uint16_t channel, const uin
         case HCI_SUBEVENT_LE_ENHANCED_CONNECTION_COMPLETE:
             att_set_db(decode_hci_le_meta_event(packet, le_meta_event_enh_create_conn_complete_t)->handle,
                        profile_data);
-            platform_calibrate_32k();
-#if (INGCHIPS_FAMILY == INGCHIPS_FAMILY_916)
-    #ifdef OPT_RAM_CODE
-            platform_config(PLATFORM_CFG_DEEP_SLEEP_TIME_REDUCTION, 4100 - 400);
-            platform_config(PLATFORM_CFG_LL_DELAY_COMPENSATION, 155);
-    #else
-            platform_config(PLATFORM_CFG_DEEP_SLEEP_TIME_REDUCTION, 5200);
-            platform_config(PLATFORM_CFG_LL_DELAY_COMPENSATION, 280);
-    #endif
-#endif
             break;
         default:
             break;
@@ -197,11 +122,11 @@ static void user_packet_handler(uint8_t packet_type, uint16_t channel, const uin
         break;
 
     case HCI_EVENT_DISCONNECTION_COMPLETE:
-        setup_adv();
         gap_set_ext_adv_enable(1, sizeof(adv_sets_en) / sizeof(adv_sets_en[0]), adv_sets_en);
         break;
 
     case ATT_EVENT_CAN_SEND_NOW:
+        // add your code
         break;
 
     case BTSTACK_EVENT_USER_MSG:
@@ -214,55 +139,16 @@ static void user_packet_handler(uint8_t packet_type, uint16_t channel, const uin
     }
 }
 
+static btstack_packet_callback_registration_t hci_event_callback_registration;
+
 uint32_t setup_profile(void *data, void *user_data)
 {
     platform_printf("setup profile\n");
-
+    // Note: security has not been enabled.
     att_server_init(att_read_callback, att_write_callback);
     hci_event_callback_registration.callback = &user_packet_handler;
     hci_add_event_handler(&hci_event_callback_registration);
     att_server_register_packet_handler(&user_packet_handler);
     return 0;
 }
-const platform_hci_link_layer_interf_t *hci_interf = NULL;
 
-static void dump_32(const void *pp, int len)
-{
-    const uint32_t *p = (const uint32_t *)pp;
-    int i;
-    for (i = 0; i < len; i++)
-        platform_printf("[%02d] = %08X\n", i, p[i]);
-    platform_printf("\n");
-}
-
-#pragma pack (push, 1)
-
-struct hci_command_packet_header
-{
-    uint16_t opcode;
-    uint8_t param_len;
-};
-
-struct hci_acl_packet_header
-{
-    uint16_t conn_handle_flags;
-    uint16_t data_length;
-};
-
-struct hci_event_packet_header
-{
-    uint8_t code;
-    uint8_t param_len;
-};
-
-#pragma pack (pop)
-
-#define DEF_NUM_OF_HCI              0x0b
-
-#if (HCI_TRANSPORT == HCI_TRANSPORT_UART)
-    #include "hci_uart.c"
-#elif (HCI_TRANSPORT == HCI_TRANSPORT_USB)
-    #include "hci_usb.c"
-#else
-    #error Unsupported HCI transport
-#endif
